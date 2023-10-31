48ec67d1dd1760250439375dd3dc4372f39e915b
Sheelu kumar <sheeluofficial@gmail.com>
2023-10-31 12:56:06 +0530

    Initial frontend setup with typescript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+       Hi
+      </div>
+  
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/main.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+import { Toaster } from 'react-hot-toast'
+import { ThemeProvider, createTheme } from "@mui/material";
+import { BrowserRouter } from 'react-router-dom'
+
+import axios from 'axios';
+
+//default axios settings
+axios.defaults.baseURL = "http://localhost:5000/api/v1";
+axios.defaults.withCredentials = true; // --> help with exchanging the cookies
+
+//Theme for the whole page - MUI
+const theme = createTheme({
+    typography: {
+        fontFamily: "Roboto Slab, serif",
+        allVariants: { color: "white" },
+    },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+            <BrowserRouter>
+                <ThemeProvider theme={theme}>
+                    <Toaster position="top-right" />
+                     <App />
+                </ThemeProvider>
+            </BrowserRouter>
+    </React.StrictMode>
+);
