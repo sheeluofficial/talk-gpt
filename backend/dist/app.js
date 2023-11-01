cfdd8669834ed914a2caca408cf76a8419493112
Sheelu kumar <sheeluofficial@gmail.com>
2023-11-02 02:16:41 +0530

    Fixing deployment issue

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { config } from "dotenv";
-import morgan from 'morgan';
 import cookieParser from "cookie-parser";
 import cors from 'cors';
 import appRouter from "./routes/index.js";
@@ -10,7 +9,7 @@ const app = express();
 //middlewares
 // Enable All CORS Requests from a Specific Origin
 const corsOptions = {
-    origin: 'https://talk-gpt-dep.vercel.app',
+    origin: process.env.CORS_ORIGIN,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     optionsSuccessStatus: 204,
@@ -19,13 +18,12 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use("/api/v1", appRouter);
-app.get("/", (req, res, next) => {
-    res.send("Yes, You are connected");
-});
+// app.get("/",(req: Request,res: Response, next : NextFunction)=>{
+//     res.send("Yes, You are connected");
+// })
 // remove it in production
 const __dirname1 = path.resolve();
-app.use(express.static(path.join(__dirname1, "../frontend/dist")));
-app.get("*", (req, res) => res.sendFile(path.resolve(__dirname1, "..", "frontend", "dist", "index.html")));
-app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname1, "public")));
+app.get("*", (req, res) => res.sendFile(path.resolve(__dirname1, "public", "index.html")));
 export default app;
 //# sourceMappingURL=app.js.map
